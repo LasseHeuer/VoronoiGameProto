a5e2046fecef4c41363d1ae38cf29f4d3f67fa6f
Lucas Weinknecht <lucasweinknecht@gmail.com>
2025-01-30 15:01:34 +0100

    Add centralized settings store.

---
+++ src/game/settings.js
@@ -0,0 +1,23 @@
+const config = {
+	"synth": {
+		"wave": "Triangle",
+		"attack": 0.05,
+		"decay": 0.2,
+		"sustain": 0.2,
+		"release": 10,
+		"cutoff": 500,
+	},
+	"dragToneVolume": null,
+	"dragToneNeighborFactor": null,
+	"colorThreshold": null,
+	"freqThreshold": null,
+	"spreadTime": null,
+	"numCells": null,
+	"pushFactor": null,
+	"pushRadius": null,
+	"boundaryMargin": null,
+	"weightInfluenceFactor": null,
+	"harmonyInfluenceFactor": null,
+	"alternateMoves": true,
+	"showFrequencyRatios": true
+}
